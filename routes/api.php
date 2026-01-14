cbb907ea65f420f6c19fc9eff8decdd48145d6ec
Eric Liao <d1044171009@gmail.com>
2026-01-15 00:02:38 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// this is a test
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
